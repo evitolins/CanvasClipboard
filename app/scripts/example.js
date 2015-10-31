ead23d682cd5445fabaa03dc56102d9f86d56811
Eriks Vitolins <evitolins@gmail.com>
2015-10-31 00:27:48 -0700

    Refactored example init

--- app/scripts/example.js
+++ app/scripts/example.js
@@ -15,36 +15,34 @@ var ctxs = {
     e : {source: canvases.e.source.getContext('2d'), dest: canvases.e.dest.getContext('2d')}
 };
 
-testImg.onload = (function(){
-    return function() {
-        ctxs.a.source.drawImage(testImg, 0, 0);
-        clipboard.cut(canvases.a.source, 10, 30, 100, 100);
-        clipboard.paste(canvases.a.dest);
+var init = function () {
+    ctxs.a.source.drawImage(testImg, 0, 0);
+    clipboard.cut(canvases.a.source, 10, 30, 100, 100);
+    clipboard.paste(canvases.a.dest);
 
-        ctxs.b.source.drawImage(testImg, 0, 0);
-        clipboard.cut(canvases.b.source, 10, 30, 100, 100);
-        clipboard.translate(130, 100);
-        clipboard.paste(canvases.b.dest);
+    ctxs.b.source.drawImage(testImg, 0, 0);
+    clipboard.cut(canvases.b.source, 10, 30, 100, 100);
+    clipboard.translate(130, 100);
+    clipboard.paste(canvases.b.dest);
 
-        ctxs.c.source.drawImage(testImg, 0, 0);
-        clipboard.cut(canvases.c.source, 10, 30, 100, 100);
-        clipboard.rotate(27);
-        clipboard.paste(canvases.c.dest);
+    ctxs.c.source.drawImage(testImg, 0, 0);
+    clipboard.cut(canvases.c.source, 10, 30, 100, 100);
+    clipboard.rotate(27);
+    clipboard.paste(canvases.c.dest);
 
+    ctxs.d.source.drawImage(testImg, 0, 0);
+    clipboard.cut(canvases.d.source, 10, 30, 100, 100);
+    clipboard.rotate(-15);
+    clipboard.scale(2);
+    clipboard.paste(canvases.d.dest);
 
-        ctxs.d.source.drawImage(testImg, 0, 0);
-        clipboard.cut(canvases.d.source, 10, 30, 100, 100);
-        clipboard.rotate(-15);
-        clipboard.scale(2);
-        clipboard.paste(canvases.d.dest);
+    ctxs.e.source.drawImage(testImg, 0, 0);
+    clipboard.cut(canvases.e.source, 10, 30, 100, 100);
+    clipboard.rotate(35);
+    clipboard.scale(2);
+    clipboard.translate(0, 50);
+    clipboard.paste(canvases.e.dest);
+};
 
-        ctxs.e.source.drawImage(testImg, 0, 0);
-        clipboard.cut(canvases.e.source, 10, 30, 100, 100);
-        clipboard.rotate(-15);
-        clipboard.scale(2);
-        clipboard.translate(20, 50);
-        clipboard.paste(canvases.e.dest);
-
-    }
-}());
+testImg.onload = init;
 testImg.src = "images/testImage.jpg";
